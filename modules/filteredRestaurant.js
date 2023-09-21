343f74d8ccb5bfbbd48b88f8eeea3b0d40ba341e
Daniel Luke Rogers <dluke.rogers@gmail.com>
2023-09-21 15:42:19 -0500

    got rid of AI API

--- modules/filteredRestaurant.js
+++ modules/filteredRestaurant.js
@@ -1,24 +1,24 @@
-'use strict';
+// 'use strict';
 
-const axios = require('axios');
-const aiAPIKey = process.env.OPENAI_API_KEY;
+// const axios = require('axios');
+// const aiAPIKey = process.env.OPENAI_API_KEY;
 
-const headers = {
-  Authorization: `Bearer ${aiAPIKey}`,
-};
+// const headers = {
+//   Authorization: `Bearer ${aiAPIKey}`,
+// };
 
-const getAIData = async (req, res) => {
-  const { criteria, data } = req.body;
-  console.log(data);
-  const model = 'gpt-3.5-turbo';
-  const prompt = `Filter the following array of restaurants that have ${criteria} on their menu: ${data}`;
-  try {
-    const response = await axios.post('https://api.openai.com/v1/chat/completions', { model, messages: [{ role: 'system', content: 'You are a helpful assistant.' }, { role: 'user', content: prompt }] }, { headers });
-    const filteredData = response.data.choices[0].message.content;
-    res.json({ filteredData });
-  } catch (e) {
-    return res.json({message: 'Something went wrong!'});
-  }
-};
+// const getAIData = async (req, res) => {
+//   const { criteria, data } = req.body;
+//   console.log(data);
+//   const model = 'gpt-3.5-turbo';
+//   const prompt = `Filter the following array of restaurants that have ${criteria} on their menu: ${data}`;
+//   try {
+//     const response = await axios.post('https://api.openai.com/v1/chat/completions', { model, messages: [{ role: 'system', content: 'You are a helpful assistant.' }, { role: 'user', content: prompt }] }, { headers });
+//     const filteredData = response.data.choices[0].message.content;
+//     res.json({ filteredData });
+//   } catch (e) {
+//     return res.json({message: 'Something went wrong!'});
+//   }
+// };
 
-module.exports = getAIData;
+// module.exports = getAIData;
